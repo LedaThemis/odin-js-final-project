055d5c8ea591e67ca806ffe939e5347c06cd7b6c
Leda <ledathemis@protonmail.com>
2022-06-08 21:28:01 +0000

    navigate to login if not logged in

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,7 +1,16 @@
 import './styles/HomePage.css';
 
+import { getAuth } from 'firebase/auth';
+import { Navigate } from 'react-router-dom';
+
 const HomePage = () => {
-  return <div className="App">Home</div>;
+  return (
+    <div className="App">
+      {!getAuth().currentUser && <Navigate to="/login" />}
+      {/* TODO: ADD HOME UI */}
+      Home
+    </div>
+  );
 };
 
 export default HomePage;
