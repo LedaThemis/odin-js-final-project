2cd85be6aa740aa6559732a5cb4e311f33c222e7
Leda <ledathemis@protonmail.com>
2022-06-08 21:29:35 +0000

    add App component

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import { AuthProvider } from './AuthContext';
+import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import LoginPage from './components/LoginPage';
+
+const App = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+  }, []);
+
+  return (
+    <BrowserRouter basename="/odin-final-project">
+      <AuthProvider value={{ currentUser }}>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+};
+
+export default App;
