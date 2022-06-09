e6f56db722a8f5e03982127fdec1187346096388
Leda <ledathemis@protonmail.com>
2022-06-09 19:09:41 +0000

    style HomeSidebar, add Twitter Logo

--- src/components/HomeSidebar.js
+++ src/components/HomeSidebar.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 
 import SidebarButton from './SidebarButton';
 
@@ -8,12 +9,14 @@ import NotificationsButtonIcon from '../assets/sidebar/NotificationsButtonIcon';
 import MessagesButtonIcon from '../assets/sidebar/MessagesButtonIcon';
 import ProfileButtonIcon from '../assets/sidebar/ProfileButtonIcon';
 import SettingsButtonIcon from '../assets/sidebar/SettingsButtonIcon';
+import TwitterLogo from '../assets/TwitterLogo';
 
 const HomeSidebar = () => {
   const [currentButtonSelected, setCurrentButtonSelected] = useState('');
 
   return (
-    <div>
+    <StyledSidebar>
+      <SidebarTwitterLogo size={2} />
       <SidebarButton
         Icon={<HomeButtonIcon size={1.75} isSelected={currentButtonSelected === 'Home'} />}
         text="Home"
@@ -44,8 +47,29 @@ const HomeSidebar = () => {
         text="Settings"
         onClick={() => setCurrentButtonSelected('Settings')}
       />
-    </div>
+    </StyledSidebar>
   );
 };
+
+const StyledSidebar = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SidebarTwitterLogo = styled(TwitterLogo)`
+  &:hover {
+    background-color: rgba(29, 155, 240, 0.1);
+  }
+
+  padding: 12px;
+
+  display: flex;
+  align-items: center;
+
+  border-radius: 9999px;
+
+  transition-property: background-color, box-shadow;
+  transition-duration: 0.2s;
+`;
 
 export default HomeSidebar;
