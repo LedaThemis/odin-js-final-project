48e4c96a4f939316983a8008cf4011979a1f84d7
Leda <ledathemis@protonmail.com>
2022-06-09 19:40:50 +0000

    add TweetButton component

---
+++ src/components/TweetButton.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import ButtonBase from './ButtonBase';
+
+const TweetButton = () => {
+  return <StyledTweetButton text="Tweet"></StyledTweetButton>;
+};
+
+const StyledTweetButton = styled(ButtonBase)`
+  &:hover {
+    background-color: rgb(26, 140, 216);
+  }
+
+  background-color: rgb(29, 155, 240);
+  border-radius: 9999px;
+
+  transition-property: background-color, box-shadow;
+  transition-duration: 0.2s;
+
+  p {
+    color: white;
+    font-weight: bold;
+  }
+`;
+
+export default TweetButton;
