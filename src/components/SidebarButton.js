66d42fe37beb9e83b4e967ea23396dd09550d252
Leda <ledathemis@protonmail.com>
2022-06-09 19:09:05 +0000

    style SidebarButton

--- src/components/SidebarButton.js
+++ src/components/SidebarButton.js
@@ -7,9 +7,22 @@ const SidebarButton = (props) => {
 };
 
 const StyledSidebarButton = styled(ButtonBase)`
-  &&:hover {
+  &:hover {
     background-color: rgba(15, 20, 25, 0.1);
   }
+
+  p {
+    margin: 0 16px 0 20px;
+  }
+  padding: 12px;
+
+  display: flex;
+  align-items: center;
+
+  border-radius: 9999px;
+
+  transition-property: background-color, box-shadow;
+  transition-duration: 0.2s;
 `;
 
 export default SidebarButton;
