021845e4d68b47fc9c8d51d2282bdebfb3d02cf9
Leda <ledathemis@protonmail.com>
2022-06-16 23:04:23 +0000

    add context menu to tweet

--- src/components/FeedTweet.js
+++ src/components/FeedTweet.js
@@ -1,8 +1,14 @@
-import defaultAvatar from '../assets/default-avatar.jpg';
-
+import { useState } from 'react';
 import styled from 'styled-components';
+import { BsTrash, BsThreeDots } from 'react-icons/bs';
+
+import defaultAvatar from '../assets/default-avatar.jpg';
+import { useAuthValue } from '../AuthContext';
 
 const FeedTweet = ({ uid, displayName, photoURL, text, tweetID }) => {
+  const [currentThreeDotsColor, setCurrentThreeDotsColor] = useState('rgb(83, 100, 113)');
+  const [isContextMenuShown, setIsContextMenuShown] = useState(false);
+  const { currentUser } = useAuthValue();
 
   const imageExists = (url) => {
     fetch(url)
@@ -18,10 +24,29 @@ const FeedTweet = ({ uid, displayName, photoURL, text, tweetID }) => {
         onerror={(e) => (e.target.src = defaultAvatar)}
       ></StyledProfileImage>
       <StyledSection>
-        <StyledProfileArea>
-          <StyledProfileName>{displayName}</StyledProfileName>
-          <StyledUID>@{uid}</StyledUID>
-        </StyledProfileArea>
+        <StyledTopSection>
+          <StyledProfileArea>
+            <StyledProfileName>{displayName}</StyledProfileName>
+            <StyledUID>@{uid}</StyledUID>
+          </StyledProfileArea>
+          {currentUser.uid === uid && (
+            <StyledContextMenuButton
+              onMouseEnter={() => setCurrentThreeDotsColor('rgb(29, 155, 240)')}
+              onMouseLeave={() => setCurrentThreeDotsColor('rgb(83, 100, 113)')}
+              onClick={() => setIsContextMenuShown((prevState) => !prevState)}
+            >
+              <BsThreeDots size={'1.5em'} color={currentThreeDotsColor} />
+              {isContextMenuShown && (
+                <StyledContextMenu>
+                    <StyledRemoveButton>
+                      <BsTrash size={'1.5em'} style={{ fill: 'red' }} />
+                      <p>Delete</p>
+                    </StyledRemoveButton>
+                </StyledContextMenu>
+              )}
+            </StyledContextMenuButton>
+          )}
+        </StyledTopSection>
         <StyledTweetText>{text}</StyledTweetText>
       </StyledSection>
     </StyledFeedTweet>
@@ -49,6 +74,12 @@ const StyledSection = styled.div`
   width: 100%;
 `;
 
+const StyledTopSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const StyledProfileArea = styled.div`
   display: flex;
   gap: 10px;
@@ -72,6 +103,60 @@ const StyledProfileName = styled.p`
 
 const StyledUID = styled.p`
   color: rgb(83, 100, 113);
+`;
+
+const StyledContextMenuButton = styled.button`
+  &:hover {
+    background-color: rgba(29, 155, 240, 0.1);
+  }
+
+  background-color: transparent;
+  border: none;
+  padding: 8px 8px;
+  border-radius: 9999px;
+  cursor: pointer;
+
+  transition-property: background-color, box-shadow;
+  transition-duration: 0.2s;
+`;
+
+const StyledContextMenu = styled.div`
+  position: absolute;
+  background-color: white;
+  box-shadow: rgba(101, 119, 134, 0.2) 0px 0px 15px 0px, rgba(101, 119, 134, 0.15) 0px 0px 3px 1px;
+  border-radius: 4px;
+
+  transform: translateX(-160px);
+
+
+`;
+
+const StyledRemoveButton = styled.button`
+  &:hover {
+    background-color: rgba(15, 20, 25, 0.1);
+  }
+
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  width: 200px;
+
+
+  p {
+    font-family: Roboto, sans-serif;
+    font-size: 1rem;
+    transform: scale(1, 0.95);
+    margin: 0;
+  }
+
+  background-color: transparent;
+  border: none;
+  padding: 8px 8px;
+  cursor: pointer;
+
+  transition-property: background-color, box-shadow;
+  transition-duration: 0.2s;
 `;
 
 const StyledTweetText = styled.p`
